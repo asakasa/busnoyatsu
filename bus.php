07e9d7a302f6449c9f434f4924559cb9acc52e2b
Kanarne <kanarne@gmail.com>
2015-05-14 23:44:39 +0900

    骨組みにすらなってない骨組み
    こういう感じ？？

---
+++ bus.php
@@ -0,0 +1,32 @@
+<?php
+
+{
+	"From":{
+		"KUTC":{
+			"to":"JR Takatsuki Station"{
+				"date":{
+					"hour":[[minute,dest,status],...]
+				}
+			}
+			"to":"JR Tonda Station"{
+				"date":{
+					"hour":[[minute,dest,status],...]
+				}
+			}
+		}
+		"JR Takatsuki Station"{
+			"to":"KUTC"{
+				"date":{
+					"hour":[[minute,dest,status],...]
+				}
+			}
+		}
+		"JR Tonda Station"{
+			"to":"KUTC"{
+				"date":{
+					"hour":[[minute,dest,status],...]
+				}
+			}
+		}
+	}
+}
