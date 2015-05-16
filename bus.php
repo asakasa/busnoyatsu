668d5d1e900f786090dbaaffec50c522ba11b7f6
Kanarne <kanarne@gmail.com>
2015-05-16 23:21:22 +0900

    JSONファイル読み込み
    読み込めてはいるようだが……

--- bus.php
+++ bus.php
@@ -1,32 +1,14 @@
 <?php
 
-{
-	"From":{
-		"KUTC":{
-			"to":"JR Takatsuki Station"{
-				"date":{
-					"hour":[[minute,dest,status],...]
-				}
-			}
-			"to":"JR Tonda Station"{
-				"date":{
-					"hour":[[minute,dest,status],...]
-				}
-			}
-		}
-		"JR Takatsuki Station"{
-			"to":"KUTC"{
-				"date":{
-					"hour":[[minute,dest,status],...]
-				}
-			}
-		}
-		"JR Tonda Station"{
-			"to":"KUTC"{
-				"date":{
-					"hour":[[minute,dest,status],...]
-				}
-			}
-		}
-	}
-}
+$kutc_tt = file_get_contents('./tt/kutc.json');
+$takatsuki_tt = file_get_contents('./tt/takatsuski.json');
+$tonda_tt = file_get_contents('./tt/tonda.json');
+
+$kutc = json_decode($kutc_tt,true);
+$takatsuki = json_decode($takatsuki_tt);
+$tonda = json_decode($tonda_tt);
+
+//test
+
+var_dump($kutc['kutc']['takatsuki']['week']);
+
