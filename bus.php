6b99b7cca81c57042e4ade79e7908fe8cf1d13ab
Kanarne <kanarne@gmail.com>
2015-05-18 01:10:40 +0900

    修正

--- bus.php
+++ bus.php
@@ -1,22 +1,25 @@
 <?php
 
-// 出発点を引数に与えたらそこの時刻表を取得する関数......になる予定
+// 出発点を引数に与えたらそこの時刻表を取得する関数
 function get_tt($location) {
 	switch ($location) {
 		case kutc:
 			$kutc_tt = file_get_contents('./tt/kutc.json');// jsonの読み込み
 			$kutc = json_decode($kutc_tt,true);// jsonの読み込み
-			$timeTable = array('kutc' => $kutc['kutc']);// 時刻業データを連想配列に
+			$time_table = array('kutc' => $kutc['kutc']);// 時刻業データを連想配列に
+			break;
 		case takatsuki:
 			$takatsuki_tt = file_get_contents('./tt/takatsuki.json');// 同上
 			$takatsuki = json_decode($takatsuki_tt,true);
-			$timeTable = array('takatsuki' => $takatsuki['takatsuki']);
+			$time_table = array('takatsuki' => $takatsuki['takatsuki']);
+			break;
 		case tonda:
 			$tonda_tt = file_get_contents('./tt/tonda.json');// 同上
 			$tonda = json_decode($tonda_tt,true);
-			$timeTable = array('tonda' => $tonda['tonda']);
+			$time_table = array('tonda' => $tonda['tonda']);
+			break;
 		default:
 			break;
 	}
-	return $timeTable
+	return $time_table;
 }
