5b6c98a3445b265f741e6cfca4302538e2017a59
Kanarne <kanarne@gmail.com>
2015-05-18 00:47:15 +0900

    無駄な記述の削除

--- bus.php
+++ bus.php
@@ -7,21 +7,16 @@ function get_tt($location) {
 			$kutc_tt = file_get_contents('./tt/kutc.json');// jsonの読み込み
 			$kutc = json_decode($kutc_tt,true);// jsonの読み込み
 			$timeTable = array('kutc' => $kutc['kutc']);// 時刻業データを連想配列に
-			return $timeTable;
-			break;
 		case takatsuki:
 			$takatsuki_tt = file_get_contents('./tt/takatsuki.json');// 同上
 			$takatsuki = json_decode($takatsuki_tt,true);
 			$timeTable = array('takatsuki' => $takatsuki['takatsuki']);
-			return $timeTable;
-			break;
 		case tonda:
 			$tonda_tt = file_get_contents('./tt/tonda.json');// 同上
 			$tonda = json_decode($tonda_tt,true);
 			$timeTable = array('tonda' => $tonda['tonda']);
-			return $timeTable;
-			break;
 		default:
 			break;
 	}
+	return $timeTable
 }
