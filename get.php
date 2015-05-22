cad9c04f28b1c6825b1f5a1044a791cf395a22fd
Kanarne <kanarne@gmail.com>
2015-05-22 15:55:30 +0900

    test

---
+++ get.php
@@ -0,0 +1,14 @@
+<html>
+<head><title>PHP TEST</title></head>
+<body>
+
+<?php
+
+$get = $_GET['test1'];
+echo "$get";
+
+?>
+
+</body>
+</html>
+
---
+++ post.php
@@ -0,0 +1,14 @@
+<html>
+<head><title>PHP TEST</title></head>
+<body>
+
+<?php
+
+$post = $_POST['test2'];
+echo "$post";
+
+?>
+
+</body>
+</html>
+
