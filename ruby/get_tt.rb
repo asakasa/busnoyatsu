772ceecd72aad435778a1911cf0c83dae1c5f573
kanarne <kanarne@gmail.com>
2015-06-28 20:06:36 +0900

    modify

--- ruby/get_tt.rb
+++ ruby/get_tt.rb
@@ -1,17 +1,8 @@
 def get_tt(location)
   require 'json'
-  case location
-  when "kutc"
-    kutc_tt = open("../tt/kutc.json") do |io|
-      JSON.load(io)
-    end
-  when "takatsuki"
-    takatsuki_tt = open("../tt/takatsuki.json") do |io|
-      JSON.load(io)
-    end
-  when "tonda"
-    tonda_tt = open("../tt/tonda.json") do |io|
-      JSON.load(io)
-    end
-  end
+  require 'rubygems'
+
+  tt_file = open("../tt/#{location}.json")
+  location = tt_file.read
+  parsed = JSON.parse(location)
 end
